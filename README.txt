1ee04eb7e44a971c16b3c97461d881b443f1d83b
Manuel Ramirez Coronel <ra_cm@outlook.com>
2023-11-14 17:45:09 -0600

    1.0.3

--- README.txt
+++ README.txt
@@ -1,10 +1,10 @@
 === QR Link Generator for WP ===
 Contributors: racmanuel
 Donate link: https://link.mercadopago.com.mx/racmanuel
-Tags: 1.0.2
+Tags: 1.0.3
 Requires at least: 5.2
 Tested up to: 6.2
-Stable tag: 1.0.2
+Stable tag: 1.0.3
 Requires PHP: 7.4
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
@@ -42,3 +42,13 @@ Visit my website: https://racmanuel.dev/
 
 
 == Upgrade Notice ==
+
+
+== Privacy Policy ==
+QR Link Generator for WP uses [Appsero](https://appsero.com) SDK to collect some telemetry data upon user's confirmation. This helps us to troubleshoot problems faster & make product improvements.
+
+Appsero SDK **does not gather any data by default.** The SDK only starts gathering basic telemetry data **when a user allows it via the admin notice**. We collect the data to ensure a great user experience for all our users.
+
+Integrating Appsero SDK **DOES NOT IMMEDIATELY** start gathering data, **without confirmation from users in any case.**
+
+Learn more about how [Appsero collects and uses this data](https://appsero.com/privacy-policy/).
--- qr-link-generator-for-wp.php
+++ qr-link-generator-for-wp.php
@@ -15,7 +15,7 @@
  * Plugin Name:       QR Link Generator for WP
  * Plugin URI:        https://plugin.com/qr-link-generator-for-wp-uri/
  * Description:       Plugin to Generate QR Code with link inserted by the user in front-end with a form.
- * Version:           1.0.2
+ * Version:           1.0.3
  * Author:            Manuel Ramirez Coronel
  * Requires at least: 5.2
  * Requires PHP:      7.4
@@ -28,26 +28,26 @@
  */
 
 // If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
-	die;
+if (!defined('WPINC')) {
+    die;
 }
 
 /**
  * Composer: autoload.php
  */
-require_once plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
+require_once plugin_dir_path(__FILE__) . 'vendor/autoload.php';
 
 /**
  * Current plugin version.
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'QR_LINK_GENERATOR_FOR_WP_VERSION', '1.0.0' );
+define('QR_LINK_GENERATOR_FOR_WP_VERSION', '1.0.3');
 
 /**
  * Define the Plugin basename
  */
-define( 'QR_LINK_GENERATOR_FOR_WP_BASE_NAME', plugin_basename( __FILE__ ) );
+define('QR_LINK_GENERATOR_FOR_WP_BASE_NAME', plugin_basename(__FILE__));
 
 /**
  * The code that runs during plugin activation.
@@ -55,9 +55,10 @@ define( 'QR_LINK_GENERATOR_FOR_WP_BASE_NAME', plugin_basename( __FILE__ ) );
  * This action is documented in includes/class-qr-link-generator-for-wp-activator.php
  * Full security checks are performed inside the class.
  */
-function qr_link_generator_for_wp_activate() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-qr-link-generator-for-wp-activator.php';
-	Qr_Link_Generator_For_Wp_Activator::activate();
+function qr_link_generator_for_wp_activate()
+{
+    require_once plugin_dir_path(__FILE__) . 'includes/class-qr-link-generator-for-wp-activator.php';
+    Qr_Link_Generator_For_Wp_Activator::activate();
 }
 
 /**
@@ -66,19 +67,37 @@ function qr_link_generator_for_wp_activate() {
  * This action is documented in includes/class-qr-link-generator-for-wp-deactivator.php
  * Full security checks are performed inside the class.
  */
-function qr_link_generator_for_wp_deactivate() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-qr-link-generator-for-wp-deactivator.php';
-	Qr_Link_Generator_For_Wp_Deactivator::deactivate();
+function qr_link_generator_for_wp_deactivate()
+{
+    require_once plugin_dir_path(__FILE__) . 'includes/class-qr-link-generator-for-wp-deactivator.php';
+    Qr_Link_Generator_For_Wp_Deactivator::deactivate();
 }
 
-register_activation_hook( __FILE__, 'qr_link_generator_for_wp_activate' );
-register_deactivation_hook( __FILE__, 'qr_link_generator_for_wp_deactivate' );
+register_activation_hook(__FILE__, 'qr_link_generator_for_wp_activate');
+register_deactivation_hook(__FILE__, 'qr_link_generator_for_wp_deactivate');
 
 /**
  * The core plugin class that is used to define internationalization,
  * admin-specific hooks, and public-facing site hooks.
  */
-require plugin_dir_path( __FILE__ ) . 'includes/class-qr-link-generator-for-wp.php';
+require plugin_dir_path(__FILE__) . 'includes/class-qr-link-generator-for-wp.php';
+
+/**
+ * Initialize the plugin tracker
+ *
+ * @return void
+ */
+function appsero_init_tracker_qr_link_generator_for_wp()
+{
+    if (!class_exists('Appsero\Client')) {
+        require_once __DIR__ . '/appsero/src/Client.php';
+    }
+    $client = new Appsero\Client('b3aea394-ea2b-4f4b-bed4-be54a9daf1fd', 'QR Link Generator for WP', __FILE__);
+    // Active insights
+    $client->insights()->init();
+
+}
+appsero_init_tracker_qr_link_generator_for_wp();
 
 /**
  * Begins execution of the plugin.
@@ -92,10 +111,11 @@ require plugin_dir_path( __FILE__ ) . 'includes/class-qr-link-generator-for-wp.p
  *
  * @since    1.0.0
  */
-function qr_link_generator_for_wp_run() {
+function qr_link_generator_for_wp_run()
+{
 
-	$plugin = new Qr_Link_Generator_For_Wp();
-	$plugin->run();
+    $plugin = new Qr_Link_Generator_For_Wp();
+    $plugin->run();
 
 }
 qr_link_generator_for_wp_run();
